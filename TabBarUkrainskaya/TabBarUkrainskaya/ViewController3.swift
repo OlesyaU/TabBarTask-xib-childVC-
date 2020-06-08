3f1dd2f15b4e0ce79767f199156e711df3189b76
Olesya <53311515+OlesyaU@users.noreply.github.com>
2020-06-08 08:55:40 +0300

    Add files via upload

---
+++ TabBarUkrainskaya/TabBarUkrainskaya/ViewController3.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController3.swift
+//  TabBarUkrainskaya
+//
+//  Created by Олеся on 05.06.2020.
+//  Copyright © 2020 Ukrainskaya Olesya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController3: UIViewController {
+    
+    @IBOutlet weak var label: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        label.numberOfLines = 0
+        label.sizeToFit()
+    }
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+}
